6c05dd212777e4b5979fe974078b7e9471d9e22a
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-06-27 22:36:13 +0300

    Create Vendor.php

---
+++ app/Http/Middleware/Vendor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Enums\Roles;
+use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
+
+class Vendor
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!\Illuminate\Support\Facades\Auth::check() || \Illuminate\Support\Facades\Auth::user()->role != Roles::Vendor){
+            return redirect()->route("home")->with('error', 'You do not have admin access.');
+        }
+
+        return $next($request);
+    }
+}
