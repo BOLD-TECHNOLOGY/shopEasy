b4e3096233886d156e8b54cb49ab660d9c5dddda
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-06-27 22:38:54 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,30 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+// user
 Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth', 'verified', 'user'])
     ->name('dashboard');
+
+// admin
+Route::view('admin/dashboard', 'admin.dashboard')
+    ->middleware(['auth', 'verified' , 'admin'])
+    ->name('admin.dashboard');
+
+// blogger
+Route::view('blogger/dashboard', 'blogger.dashboard')
+    ->middleware(['auth', 'verified' , 'blogger'])
+    ->name('blogger.dashboard');
+
+// vendor
+Route::view('vendor/dashboard', 'vendor.dashboard')
+    ->middleware(['auth', 'verified' , 'vendor'])
+    ->name('vendor.dashboard');
+
+// customer
+Route::view('customer/dashboard', 'customer.dashboard')
+    ->middleware(['auth', 'verified' , 'customer'])
+    ->name('customer.dashboard');
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
