ee1ddc6e6482a5f97660a222d4ad76fa930ed6e3
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-06-27 22:36:25 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,14 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use App\Enums\Roles;
+
+use App\Http\Middleware\UserMiddleware;
+use App\Http\Middleware\Customer;
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\Vendor;
+use App\Http\Middleware\Blogger;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -11,7 +19,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            "admin" => Admin::class,
+            "user" => UserMiddleware::class,
+            "blogger" =>Blogger::class,
+            "vendor" =>Vendor::class,
+            "customer" =>Customer::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
