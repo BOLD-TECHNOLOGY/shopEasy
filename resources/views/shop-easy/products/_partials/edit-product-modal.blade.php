32833fcb54175f6dd4342e2e176695c3046f1c5c
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-07-14 21:26:36 +0300

    Update edit-product-modal.blade.php

--- resources/views/shop-easy/products/_partials/edit-product-modal.blade.php
+++ resources/views/shop-easy/products/_partials/edit-product-modal.blade.php
@@ -1,19 +1,41 @@
-<x-modal id="editProductModal{{ $product->id }}" title="Edit Product" size="modal-lg">
-    <form action="{{ route('products.update', $product) }}" method="POST">
-      @csrf @method('PUT')
+<x-modal id="editProductModal{{ $product->id }}" title="{{ $product->name }} Details" size="modal-lg">
 
-      <div class="modal-content">
-        <div class="modal-header"><h5>Edit Product</h5></div>
-        <div class="modal-body">
-            <input name="name" value="{{ $product->name }}" required>
-            <textarea name="description">{{ $product->description }}</textarea>
-            <input name="price" type="number" step="0.01" value="{{ $product->price }}" required>
-        </div>
-        <div class="modal-footer">
-            <button type="submit">Update</button>
-        </div>
-      </div>
+    <form method="POST" action="{{ route('products.update', $product) }}">
+        @csrf
+        @method('PUT')
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Product: {{ $product->name }}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body">
+                <div>
+                    <label>Name</label>
+                    <input type="text" name="name" class="form-control" value="{{ $product->name }}">
+                </div>
+                <div>
+                    <label>Price</label>
+                    <input type="number" name="price" class="form-control" value="{{ $product->price }}">
+                </div>
+                <div>
+                    <label>Description</label>
+                    <textarea name="description" class="form-control">{{ $product->description }}</textarea>
+                </div>
+                <div>
+                    <label>Shop</label>
+                    <input type="text" class="form-control" value="{{ $product->shop->name }}" readonly>
+                </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+              <button type="submit" class="btn btn-primary">Save Changes</button>
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          </div>
     </form>
-    
+    <form action="{{ route('products.destroy', $product) }}" method="POST" style="display:inline;">
+      @csrf @method('DELETE')
+      <button type="submit" class="btn btn-danger">Delete</button>
+    </form>
+
 </x-modal>
 <script src="{{ asset('assets/js/modals.js') }}"></script>
