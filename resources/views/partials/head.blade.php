376c865f06cd6450daa34c0cf47c9be5b35d04c8
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-07-14 21:26:29 +0300

    Update head.blade.php

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -25,5 +25,7 @@
 
 <!-- Alpine.js -->
 <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 
 @fluxAppearance
