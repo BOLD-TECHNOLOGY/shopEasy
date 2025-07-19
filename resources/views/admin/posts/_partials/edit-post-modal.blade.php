e57e4f96b16c6fd2c0670379f0c26d3bc1479982
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-07-19 08:55:27 +0300

    Update edit-post-modal.blade.php

--- resources/views/admin/posts/_partials/edit-post-modal.blade.php
+++ resources/views/admin/posts/_partials/edit-post-modal.blade.php
@@ -1,12 +1,35 @@
 <x-modal id="editPostModal{{ $post->id }}" title="Edit Post" size="modal-lg">
-
     <form action="{{ route('admin.posts.update', $post) }}" method="POST">
-        @csrf @method('PUT')
-        <input name="title" class="form-control mb-2" value="{{ $post->title }}" required>
-        <textarea name="content" class="form-control mb-2" rows="4" required>{{ $post->content }}</textarea>
-        <button type="submit" class="btn btn-primary">Update</button>
-    </form>
+        @csrf
+        @method('PUT')
 
+        <div class="modal-body">
+            <div class="mb-3">
+                <label for="title{{ $post->id }}" class="form-label">Post Title</label>
+                <input 
+                    type="text" 
+                    name="title" 
+                    id="title{{ $post->id }}" 
+                    class="form-control" 
+                    value="{{ $post->title }}" 
+                    required>
+            </div>
+
+            <div class="mb-3">
+                <label for="content{{ $post->id }}" class="form-label">Content</label>
+                <textarea 
+                    name="content" 
+                    id="content{{ $post->id }}" 
+                    class="form-control" 
+                    rows="5" 
+                    required>{{ $post->content }}</textarea>
+            </div>
+        </div>
+
+        <div class="modal-footer">
+            <button type="submit" class="btn btn-primary">Update Post</button>
+        </div>
+    </form>
 </x-modal>
 
 <script src="{{ asset('assets/js/modals.js') }}"></script>
