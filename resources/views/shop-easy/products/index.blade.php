0ee1614578c68d176b9b01ffe97a0fd33604110a
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-07-19 08:56:00 +0300

    Update index.blade.php

--- resources/views/shop-easy/products/index.blade.php
+++ resources/views/shop-easy/products/index.blade.php
@@ -1,45 +1,68 @@
 <x-layouts.app :title="__('eliteShop | Products')">
     @include ('shop-easy.includes.dashboard-head')
 
-    <div class="container">
-        <h2>All Products from My Shops</h2>
+    <div class="container mt-4">
+        <h2 class="mb-3">All Products from My Shops</h2>
 
         @if(session('success'))
             <div class="alert alert-success">{{ session('success') }}</div>
         @endif
 
-        <table class="table table-striped table-bordered">
-            <thead>
-                <tr>
-                    <th>S/N</th>
-                    <th>Product Name</th>
-                    <th>Shop Name</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($products as $index => $product)
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered align-middle">
+                <thead class="table-dark">
                     <tr>
-                        <td>{{ $products->firstItem() + $index }}</td>
-                        <td>{{ $product->name }}</td>
-                        <td>{{ $product->shop->name }}</td>
-                        <td>
-                            <button data-bs-toggle="modal" data-bs-target="#editProductModal{{ $product->id }}">
-                                View
-                            </button>
-                        </td>
+                        <th>#</th>
+                        <th>Image</th>
+                        <th>Product Name</th>
+                        <th>Price</th>
+                        <th>Stock</th>
+                        <th>Shop</th>
+                        <th>Action</th>
                     </tr>
-                    
-                    @include('shop-easy.products._partials.edit-product-modal', ['product' => $product])
-                @empty
-                    <tr>
-                        <td colspan="4">No products found.</td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @forelse ($products as $index => $product)
+                        <tr>
+                            <td>{{ $products->firstItem() + $index }}</td>
+                            <td>
+                                @if($product->thumbnail)
+                                    <img src="{{ asset('storage/' . $product->thumbnail) }}" alt="{{ $product->name }}" width="50" height="50" style="object-fit: cover;">
+                                @else
+                                    <span class="text-muted">N/A</span>
+                                @endif
+                            </td>
+                            <td>{{ $product->name }}</td>
+                            <td>
+                                <strong>${{ number_format($product->price, 2) }}</strong>
+                                @if($product->on_sale && $product->sale_price)
+                                    <br><span class="text-danger">${{ number_format($product->sale_price, 2) }}</span>
+                                @endif
+                            </td>
+                            <td>{{ $product->stock ?? 'N/A' }}</td>
+                            <td>{{ $product->shop->name }}</td>
+                            <td>
+                                <button class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#editProductModal{{ $product->id }}">
+                                    View/Edit
+                                </button>
+                            </td>
+                        </tr>
 
-        {{ $products->links() }}
+                        {{-- Include Modal --}}
+                        @include('shop-easy.products._partials.edit-product-modal', ['product' => $product])
+                    @empty
+                        <tr>
+                            <td colspan="7" class="text-center">No products found.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+
+        <!-- Pagination -->
+        <div class="mt-3">
+            {{ $products->links() }}
+        </div>
     </div>
 </x-layouts.app>
 
