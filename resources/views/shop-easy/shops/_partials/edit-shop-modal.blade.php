9d2416b8744636ec2279261ce89d2bc6c0633f26
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-07-19 08:56:16 +0300

    Update edit-shop-modal.blade.php

--- resources/views/shop-easy/shops/_partials/edit-shop-modal.blade.php
+++ resources/views/shop-easy/shops/_partials/edit-shop-modal.blade.php
@@ -1,20 +1,37 @@
 <x-modal id="editShopModal{{ $shop->id }}" title="Edit Shop" size="modal-lg">
-    
     <form action="{{ route('shops.update', $shop) }}" method="POST">
-        @csrf @method('PUT')
+        @csrf
+        @method('PUT')
+
         <div class="modal-content">
-            <div class="modal-header"><h5>Edit Shop</h5></div>
-            <div class="modal-body">
-                <input name="name" value="{{ $shop->name }}" required>
-                <input name="category" value="{{ $shop->category }}">
-                <textarea name="description">{{ $shop->description }}</textarea>
+            <div class="modal-header">
+                <h5 class="modal-title">Edit Shop - {{ $shop->name }}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
+
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="shopName{{ $shop->id }}" class="form-label">Shop Name</label>
+                    <input type="text" name="name" id="shopName{{ $shop->id }}" class="form-control" value="{{ $shop->name }}" required>
+                </div>
+
+                <div class="mb-3">
+                    <label for="shopCategory{{ $shop->id }}" class="form-label">Category <small class="text-muted">(optional)</small></label>
+                    <input type="text" name="category" id="shopCategory{{ $shop->id }}" class="form-control" value="{{ $shop->category }}">
+                </div>
+
+                <div class="mb-3">
+                    <label for="shopDescription{{ $shop->id }}" class="form-label">Description</label>
+                    <textarea name="description" id="shopDescription{{ $shop->id }}" class="form-control" rows="3">{{ $shop->description }}</textarea>
+                </div>
+            </div>
+
             <div class="modal-footer">
-                <button type="submit">Update</button>
+                <button type="submit" class="btn btn-primary">Update Shop</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
             </div>
         </div>
     </form>
-
 </x-modal>
 
 <script src="{{ asset('assets/js/modals.js') }}"></script>
