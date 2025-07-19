6b9b45a1c5ae29551af255989d3f84dcb209f95c
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-07-19 08:55:36 +0300

    Update index.blade.php

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,8 +1,8 @@
 <x-layouts.app :title="__('eliteShop | Posts')">
-    @include ('shop-easy.includes.dashboard-head')
+    @include('shop-easy.includes.dashboard-head')
 
-    <div class="container">
-        <h2>Promotion Posts</h2>
+    <div class="container py-4">
+        <h2 class="mb-4">Promotion Posts</h2>
 
         @if(session('success'))
             <div class="alert alert-success">{{ session('success') }}</div>
@@ -12,43 +12,57 @@
             Create New Post
         </button>
 
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>S/N</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Date</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse($posts as $index => $post)
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover align-middle">
+                <thead class="table-dark">
                     <tr>
-                        <td>{{ $index + 1 }}</td>
-                        <td>{{ $post->title }}</td>
-                        <td>{{ $post->user->name }}</td>
-                        <td>{{ $post->created_at->format('Y-m-d') }}</td>
-                        <td>
-                            <button class="btn btn-sm btn-info" data-bs-toggle="modal" data-bs-target="#editPostModal{{ $post->id }}">
-                                View
-                            </button>
-
-                            <form action="{{ route('admin.posts.destroy', $post) }}" method="POST" style="display:inline;">
-                                @csrf @method('DELETE')
-                                <button type="submit" onclick="return confirm('Delete this post?')" class="btn btn-sm btn-danger">Delete</button>
-                            </form>
-                        </td>
+                        <th>S/N</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Date</th>
+                        <th class="text-center">Actions</th>
                     </tr>
+                </thead>
+                <tbody>
+                    @forelse($posts as $index => $post)
+                        <tr>
+                            <td>{{ $posts->firstItem() + $index }}</td>
+                            <td>{{ $post->title }}</td>
+                            <td>{{ $post->user->name }}</td>
+                            <td>{{ $post->created_at->format('Y-m-d') }}</td>
+                            <td class="text-center">
+                                <button class="btn btn-sm btn-info" data-bs-toggle="modal" data-bs-target="#editPostModal{{ $post->id }}">
+                                    View
+                                </button>
 
-                    @include('admin.posts._partials.edit-post-modal', ['post' => $post])
-                @empty
-                    <tr><td colspan="5">No posts yet.</td></tr>
-                @endforelse
-            </tbody>
-        </table>
+                                <form action="{{ route('admin.posts.destroy', $post) }}" method="POST" style="display:inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button 
+                                        type="submit" 
+                                        class="btn btn-sm btn-danger" 
+                                        onclick="return confirm('Are you sure you want to delete this post?')">
+                                        Delete
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+
+                        @include('admin.posts._partials.edit-post-modal', ['post' => $post])
+                    @empty
+                        <tr>
+                            <td colspan="5" class="text-center">No posts yet.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+
+        {{-- Pagination --}}
+        <div class="mt-3">
+            {{ $posts->links() }}
+        </div>
     </div>
 
     @include('admin.posts._partials.add-post-modal')
-
 </x-layouts.app>
